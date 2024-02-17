775451da1d3b45f3bbccd8d1e8d2542a26b65d5a
Shubham Jain <157024874+jshub29@users.noreply.github.com>
2024-02-17 18:03:11 +0530

    Update const.py

--- const.py
+++ const.py
@@ -1,1 +1,6 @@
-
+#============== Script Constants===============
+r1 = 7.14/100          #1 month Term MIBOR        Source: https://www.fbil.org.in/#/home
+r2 = 7.35/100         # 3 Month Term MIBOR
+N30 = 43200             # No. of minutes in 30 days
+N365 = 525600           # No. of minutes in 365 day-year
+twoStrikeDiff = 100    # 50 for Nifty, 100 for Bank Nifty
