ed285ed22f4e997a8423428dbe537b6db1a75bfc
Shubham Jain <157024874+jshub29@users.noreply.github.com>
2024-02-17 18:02:50 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,12 +11,9 @@ import json
 import pandas as pd
 from datetime import datetime
 
-#============== Script Constants===============
-r1 = 7.14/100          #1 month Term MIBOR        Source: https://www.fbil.org.in/#/home
-r2 = 7.35/100         # 3 Month Term MIBOR
-N30 = 43200             # No. of minutes in 30 days
-N365 = 525600           # No. of minutes in 365 day-year
-twoStrikeDiff = 100    # 50 for Nifty, 100 for Bank Nifty
+from const import *
+
+
 
 #======================== functions ============
 
